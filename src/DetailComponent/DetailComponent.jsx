b2f11d4aeccae5fdfdcc530b60ffdea3d8122855
JosueSifuentesA <contactojosuesifuentes@gmail.com>
2023-02-06 22:08:50 -0500

    Add styles for desktop

--- src/DetailComponent/DetailComponent.jsx
+++ src/DetailComponent/DetailComponent.jsx
@@ -19,33 +19,34 @@ const DetailComponent = () => {
           statistics dashboard.
         </p>
       </div>
+      <div className="detailComponent_statistics">
+        <DetailStatisticHandler
+          img={iconRecognition}
+          title={"Brand Recognition"}
+          paragraph={
+            "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
+          }
+          altIcon="Icon of grafic records"
+        />
 
-      <DetailStatisticHandler
-        img={iconRecognition}
-        title={"Brand Recognition"}
-        paragraph={
-          "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content."
-        }
-        altIcon="Icon of grafic records"
-      />
+        <DetailStatisticHandler
+          img={iconDetailedRecords}
+          title={"Detailed Records"}
+          paragraph={
+            "Gain insights into who is clicking your links.Knowing when and where people engage with your content helps inform better decisions."
+          }
+          altIcon={"Icon of a speedometer"}
+        />
 
-      <DetailStatisticHandler
-        img={iconDetailedRecords}
-        title={"Detailed Records"}
-        paragraph={
-          "Gain insights into who is clicking your links.Knowing when and where people engage with your content helps inform better decisions."
-        }
-        altIcon={"Icon of a speedometer"}
-      />
-
-      <DetailStatisticHandler
-        img={iconFullyCustomizable}
-        title={"Fully Customizable"}
-        paragraph={
-          "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
-        }
-        altIcon={"Icon of a group of art tools"}
-      />
+        <DetailStatisticHandler
+          img={iconFullyCustomizable}
+          title={"Fully Customizable"}
+          paragraph={
+            "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+          }
+          altIcon={"Icon of a group of art tools"}
+        />
+      </div>
     </section>
   );
 };
--- src/DetailStatisticHandler/DetailStatisticHandler.jsx
+++ src/DetailStatisticHandler/DetailStatisticHandler.jsx
@@ -1,8 +1,13 @@
 import "./detailStatisticHandler.css";
 
 const DetailStatisticHandler = ({ img, title, paragraph, altIcon }) => {
+  const classTitle = title.split(" ");
+  const classTitleTogether = `${classTitle[0]}${classTitle[1]}`;
+
   return (
-    <div className="DetailStatisticHandler">
+    <div
+      className={`DetailStatisticHandler DetailStatisticHandler__${classTitleTogether}`}
+    >
       <img className="DetailStatisticHandler_img" src={img} alt={altIcon} />
       <h1 className="DetailStatisticHandler_h1">{title}</h1>
       <p className="DetailStatisticHandler_paragraph">{paragraph}</p>
